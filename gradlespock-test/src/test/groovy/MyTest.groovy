7d0ce8c2dbc6d386256e856151c4369b8dc0243d
mgafsia <mohamed.gafsia@gmail.com>
2017-05-19 09:02:59 +0200

    Add gradle spock module.

---
+++ gradlespock-test/src/test/groovy/MyTest.groovy
@@ -0,0 +1,16 @@
+import spock.lang.Specification
+
+/**
+ * Created by slah on 17/05/2017.
+ */
+class MyTest extends Specification {
+    def "my test"() {
+        given:
+          List<Integer> list = new ArrayList<>()
+        when:
+            list.add(1)
+        then:
+            2 == list.get(0)
+
+    }
+}
